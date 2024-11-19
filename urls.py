0dfac7f301e68bf678e4c13efa857134dc1e8264
nsmsaran <161919167+nsmsaran@users.noreply.github.com>
2024-11-19 20:40:48 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -5,5 +5,14 @@ from triumph import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.landing),
-    path('index',views.index)
+    path('index',views.index),
+    path('todo',views.todo),
+    path('articles',views.articles),
+    path('journal',views.journal),
+    path('articlepage',views.articlepage),
+    path('level',views.level),
+    path('levelsecond',views.levelsecond),
+    path('quiz',views.quiz),
+    path('minitube',views.minitube),
+    path('podcast',views.podcast)
 ]
